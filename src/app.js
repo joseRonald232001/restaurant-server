02362c99f10255c5ef0cb0b6c8c832b914834a82
joseRonald232001 <phjronald@gmail.com>
2024-11-15 10:54:09 -0400

    integracion de cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const productoRoutes = require("./routes/productoRoutes"); 
 const employeeRoutes = require("./routes/employeeRoutes");
 const app = express();
 const port = process.env.PORT || 3000;
 // Middleware para parsear JSON
 app.use(bodyParser.json());
+app.use(cors());
 
 // Usar las rutas de productos
 app.use("/api", productoRoutes);
